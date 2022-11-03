beb53a41f716a9c23539d2725993c0d39e995369
Leonid Khitskov <ivanovichivanivanov@protonmail.ch>
2022-11-03 09:08:42 +0900

    fix: handle error in first subtask

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,4 +2,5 @@ const fs = require('fs');
 const path = require('path');
 const { stdout } = process;
 const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+readableStream.on('error', err => console.log(err.message));
 readableStream.on('data', chunk => stdout.write(chunk));
