3c71fa4bc6faa43037bc8eed4055ade0f8c80737
Leonid Khitskov <ivanovichivanivanov@protonmail.ch>
2022-11-04 09:06:14 +0900

    feat: complete fourth subtask

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const fsPr = require('fs/promises');
+const path = require('path');
+const { stdout } = process;
 
+const copyDir = (origin, copy) => {
+  fsPr.mkdir(path.join(__dirname, copy), { recursive: true })
+  .then(result => {
+    if (result) {
+      stdout.write(`Copy folder successfully created`);
+    } else stdout.write('Copy folder successfully updated');
+  })
+  .catch(err => console.log(err.message));
+
+  const root = __dirname;
+  const folderNames = [origin];
+  const copyFolderNames = [copy];
+
+  const copyFolder = (folderNames, copyFolderNames) => {
+    fsPr.readdir(path.join(root, ...folderNames), { withFileTypes: true })
+      .then(data => data.forEach(el => {
+        if (el.isFile()) {
+          fsPr.copyFile(path.join(root, ...folderNames, el.name), path.join(root, ...copyFolderNames, el.name))
+            .catch(err => console.log(err.message));
+        }
+        if (el.isDirectory()) {
+          const originPath = [...folderNames, el.name];
+          const copyPath = [...copyFolderNames, el.name];
+          fsPr.mkdir(path.join(root, ...copyPath), { recursive: true })
+            .catch(err => console.log(err.message));
+
+          copyFolder(originPath, copyPath);
+        }
+      }))
+      .catch(err => console.log(err.message));
+  }
+  copyFolder(folderNames, copyFolderNames);
+}
+
+copyDir('files', 'files-copy');
