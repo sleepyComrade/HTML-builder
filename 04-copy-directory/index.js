883620ae4e05d9338d08c66c0d8b9b2bfcf59254
Leonid Khitskov <ivanovichivanivanov@protonmail.ch>
2022-11-06 22:18:13 +0900

    fix: rewrite fourth subtask

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,51 +2,68 @@ const fsPr = require('fs/promises');
 const path = require('path');
 const { stdout } = process;
 
-const copyDir = async (origin, copy) => {
+const copyDir = (origin, copy) => {
   const root = __dirname;
   const folderNames = [origin];
   const copyFolderNames = [copy];
-
-  const copyFolder = async (folderNames, copyFolderNames) => {
-    fsPr.readdir(path.join(root, ...folderNames), { withFileTypes: true })
-      .then(data => data.forEach(el => {
-        if (el.isFile()) {
-          fsPr.copyFile(path.join(root, ...folderNames, el.name), path.join(root, ...copyFolderNames, el.name))
-            .catch(err => console.log(err.message));
-        }
-        if (el.isDirectory()) {
-          const originPath = [...folderNames, el.name];
-          const copyPath = [...copyFolderNames, el.name];
-          fsPr.mkdir(path.join(root, ...copyPath), { recursive: true })
-            .then(() => copyFolder(originPath, copyPath))
-            .catch(err => console.log(err.message));
-        }
-      }))
+  
+  function copyItem(item, folderNames, copyFolderNames, onCopy){
+    fsPr.copyFile(path.join(root, ...folderNames, item.name), path.join(root, ...copyFolderNames, item.name))
       .catch(err => console.log(err.message));
+    onCopy();
+  }
+
+  const iterateFolder = (arr, folderNames, copyFolderNames, onEnd) => {
+    const array = [...arr].reverse();
+
+    const copyFile = (current, onCopy) => {
+      if (current.isFile()) {
+        copyItem(current, folderNames, copyFolderNames, onCopy)
+      }
+      if (current.isDirectory()) {
+        const originPath = [...folderNames, current.name];
+        const copyPath = [...copyFolderNames, current.name];
+        fsPr.mkdir(path.join(root, ...copyPath), { recursive: true })
+          .then(() => copyFolder(originPath, copyPath, onCopy))
+          .catch(err => console.log(err.message));
+      }
+    }
+
+    const copyDirectory = (arr, onEnd) => {
+      if (arr.length) {
+        const current = arr.pop();
+        copyFile(current, () => copyDirectory(arr, onEnd));
+      } else {
+        onEnd();
+      }
+    }
+    copyDirectory(array, onEnd);
+  }
+  
+  const copyFolder = (folderNames, copyFolderNames, onEnd) => {
+    fsPr.readdir(path.join(root, ...folderNames), { withFileTypes: true })
+    .then(data => {
+      const files = data;
+      iterateFolder(files, folderNames, copyFolderNames, onEnd);
+    })
   }
 
   fsPr.mkdir(path.join(__dirname, copy), { recursive: true })
-  .then(result => {
-    if (result) {
-      copyFolder(folderNames, copyFolderNames)
-        .then(() => stdout.write(`Copy folder successfully created`))
-        .catch(err => console.log(err.message));
-    } else {
-      fsPr.rm(path.join(__dirname, copy), { recursive: true, force: true })
-        .then(() => {
-          fsPr.mkdir(path.join(__dirname, copy), { recursive: true })
-            .then(() => {
-              copyFolder(folderNames, copyFolderNames)
-                .then(() => stdout.write(`Copy folder successfully updated`))
-                .catch(err => console.log(err.message));
-            })
-            .catch(err => console.log(err.message));
-        })
-        .catch(err => console.log(err.message));
-    };
-  })
-  .catch(err => console.log(err.message));
+    .then(result => {
+      if (result) {
+        copyFolder(folderNames, copyFolderNames, () => stdout.write('Copy folder successfully created'));
+      }
+      else {
+        fsPr.rm(path.join(__dirname, copy), { recursive: true, force: true })
+          .then(() => {
+            fsPr.mkdir(path.join(__dirname, copy), { recursive: true })
+              .then(() => copyFolder(folderNames, copyFolderNames, () => stdout.write('Copy folder successfully updated')))
+              .catch(err => console.log(err.message));
+          })
+          .catch(err => console.log(err.message));
+      };
+    })
+    .catch(err => console.log(err.message));
 }
 
-copyDir('files', 'files-copy')
-  .catch(err => console.log(err.message));
+copyDir('files', 'files-copy');
